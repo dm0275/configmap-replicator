c7b30901f9d57fa351bea93c38e37f9de8b96d47
Daniel Mancilla <dm0275@gmail.com>
2023-10-25 11:53:16 -0500

    Removed boilerplat and updated project

--- main.go
+++ main.go
@@ -1,104 +1,80 @@
-/*
-Copyright 2023.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package main
 
 import (
-	"flag"
+	"context"
+	"fmt"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
 	"os"
-
-	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
-	// to ensure that exec-entrypoint and run can make use of them.
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
-
-	"k8s.io/apimachinery/pkg/runtime"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/healthz"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-	//+kubebuilder:scaffold:imports
 )
-
-var (
-	scheme   = runtime.NewScheme()
-	setupLog = ctrl.Log.WithName("setup")
-)
-
-func init() {
-	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
-
-	//+kubebuilder:scaffold:scheme
-}
 
 func main() {
-	var metricsAddr string
-	var enableLeaderElection bool
-	var probeAddr string
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
-		"Enable leader election for controller manager. "+
-			"Enabling this will ensure there is only one active controller manager.")
-	opts := zap.Options{
-		Development: true,
-	}
-	opts.BindFlags(flag.CommandLine)
-	flag.Parse()
-
-	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
-
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:                 scheme,
-		MetricsBindAddress:     metricsAddr,
-		Port:                   9443,
-		HealthProbeBindAddress: probeAddr,
-		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "9daed535.fussionlabs",
-		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
-		// when the Manager ends. This requires the binary to immediately end when the
-		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
-		// speeds up voluntary leader transitions as the new leader don't have to wait
-		// LeaseDuration time first.
-		//
-		// In the default scaffold provided, the program ends immediately after
-		// the manager stops, so would be fine to enable this option. However,
-		// if you are doing or is intended to do any operation such as perform cleanups
-		// after the manager stops then its usage might be unsafe.
-		// LeaderElectionReleaseOnCancel: true,
-	})
+	// Load Kubernetes configuration from the default location or from a kubeconfig file.
+	config, err := rest.InClusterConfig()
 	if err != nil {
-		setupLog.Error(err, "unable to start manager")
+		fmt.Fprintf(os.Stderr, "Error loading in-cluster config: %v\n", err)
 		os.Exit(1)
 	}
 
-	//+kubebuilder:scaffold:builder
-
-	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
-		setupLog.Error(err, "unable to set up health check")
-		os.Exit(1)
-	}
-	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
-		setupLog.Error(err, "unable to set up ready check")
+	// Create a Kubernetes clientset.
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating Kubernetes clientset: %v\n", err)
 		os.Exit(1)
 	}
 
-	setupLog.Info("starting manager")
-	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
-		setupLog.Error(err, "problem running manager")
+	// Create a ConfigMap controller.
+	controller := NewConfigMapReplicatorController(clientset)
+
+	// Start the controller.
+	if err := controller.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error running controller: %v\n", err)
 		os.Exit(1)
 	}
+}
+
+// ConfigMapReplicatorController is responsible for replicating ConfigMaps.
+type ConfigMapReplicatorController struct {
+	clientset *kubernetes.Clientset
+}
+
+// NewConfigMapReplicatorController creates a new instance of the ConfigMapReplicatorController.
+func NewConfigMapReplicatorController(clientset *kubernetes.Clientset) *ConfigMapReplicatorController {
+	return &ConfigMapReplicatorController{clientset: clientset}
+}
+
+// Run starts the controller and watches for ConfigMap changes.
+func (c *ConfigMapReplicatorController) Run() error {
+	// Set up a ConfigMap watcher.
+	configMapListWatcher := cache.NewListWatchFromClient(
+		c.clientset.CoreV1().RESTClient(),
+		"configmaps",
+		"source-namespace", // Change to your source namespace.
+		cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				// Replicate the ConfigMap to target namespaces.
+				// Add your replication logic here.
+			},
+			UpdateFunc: func(oldObj, newObj interface{}) {
+				// Handle updates to ConfigMaps.
+			},
+			DeleteFunc: func(obj interface{}) {
+				// Handle ConfigMap deletions if necessary.
+			},
+		},
+	)
+
+	// Set up a shared informer and run it in the background.
+	sharedInformer := cache.NewSharedInformer(configMapListWatcher, &v1.ConfigMap{}, 0)
+	go sharedInformer.Run(context.Background().Done())
+
+	// Wait for the informer to sync.
+	if !cache.WaitForCacheSync(context.Background().Done(), sharedInformer.HasSynced) {
+		return fmt.Errorf("failed to sync informer cache")
+	}
+
+	// Block the main goroutine to keep the controller running.
+	select {}
 }
